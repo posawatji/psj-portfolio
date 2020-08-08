291f98be48c7576c92ade059b54c031589c6f337
jamesboy888 <jamesboyasd@gmail.com>
2020-08-09 01:13:28 +0700

    new package reveal and new footer contact

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Education from "./components/education";
 import Intern from "./components/intern";
 import Skills from "./components/skills";
 import About from "./components/about";
+import Fade from 'react-reveal/Fade';
 
 function App() {
   return (
@@ -21,32 +22,48 @@ function App() {
           dark={false}
           id="section1"
         />
+        <Fade bottom>
         <Section
           title="About"
           subtitle={<About />}
           dark={true}
           id="section2"
         />
+        </Fade>
+        <Fade bottom>
         <Section
           title="Education"
           subtitle={<Education />}
           dark={false}
           id="section3"
         />
+        </Fade>
+        <Fade bottom>
         <Section
           title="Intern"
           subtitle={<Intern />}
           dark={true}
           id="section4"
         />
+        </Fade>
+        <Fade bottom>
         <Section
           title="Skills"
           subtitle={<Skills/>}
           dark={false}
           id="section5"
         />
+        </Fade>
+        <Fade bottom>
         <Section
           title="Contact"
+          subtitle={<Contact />}
+          dark={true}
+          id="section6"
+        />
+        </Fade>
+        <Section
+          title="footer"
           subtitle={<Contact />}
           dark={true}
           id="section6"
--- src/components/contact.js
+++ src/components/contact.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import "./css/contact.css"
+import { FaFacebookSquare, FaGithubSquare, FaLinkedin } from 'react-icons/fa';
 
 export default function Contact() {
     return (
         <div className="contactPSJ">
-             Email: posawatji@gmail.com 
+             <h3>Email: posawatji@gmail.com</h3>
+             <a href="https://www.facebook.com/jamesboy888" style={{"margin-left":"4rem" }}><FaFacebookSquare color="#fff" size="3rem" /></a>
+             <a href="https://github.com/jamesboy888" style={{"margin-left":"1rem" }}><FaGithubSquare color="#fff" size="3rem" /></a>
+             <a href="https://www.linkedin.com/in/posawat-jirawutworanat-29451019b/" style={{"margin-left":"1rem"}}><FaLinkedin color="#fff" size="3rem" /></a>
+              
+             <h4 style={{"margin-top":"5%", color: "white", "text-align": "end" }}>© Copyright 2020. All Rights Reserved.</h4>
         </div>
     )
 }
