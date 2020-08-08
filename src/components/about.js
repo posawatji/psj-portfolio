572680ea340a9c711514749e53904e2e24ef8b67
jamesboy888 <jamesboyasd@gmail.com>
2020-08-08 20:17:59 +0700

    update and install react icon package

--- src/components/about.js
+++ src/components/about.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
+
 export default function about() {
+
     return (
         <div>
-            I am Posawat jirawutworanat.
-I am new graduate who interested in Web Development. I like to learn about new technologies. I am a self taught Full Stack Web Developer. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.
-
+            I am Posawat Jirawutworanat.
+            I am new graduate who interested in Web Development. I like to learn about new technologies. I am a self taught Full Stack Web Developer. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.
+            
         </div>
     )
 }
