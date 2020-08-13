83e4ab7958f5db38c87fa6e3e4ff2001172d4e11
jamesboy888 <jamesboyasd@gmail.com>
2020-08-13 20:13:22 +0700

    Updates

---
+++ precache-manifest.152a920c686d365f115f5400a5a54a8c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "200b7b08d7618285ede0a98378cb7474",
+    "url": "/psj-portfolio/index.html"
+  },
+  {
+    "revision": "d0279bd786306581cddd",
+    "url": "/psj-portfolio/static/css/main.d40a0560.chunk.css"
+  },
+  {
+    "revision": "09d2cca65b2a0b79699a",
+    "url": "/psj-portfolio/static/js/2.42121b37.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/psj-portfolio/static/js/2.42121b37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0279bd786306581cddd",
+    "url": "/psj-portfolio/static/js/main.faf56870.chunk.js"
+  },
+  {
+    "revision": "fb11543fe62a230b182a",
+    "url": "/psj-portfolio/static/js/runtime-main.f570ef20.js"
+  },
+  {
+    "revision": "c9da963791576d3b16156ea09500ca52",
+    "url": "/psj-portfolio/static/media/blueseasLogo.c9da9637.svg"
+  },
+  {
+    "revision": "0c0557d90ccd7edd7bd5bbb7750ffcc0",
+    "url": "/psj-portfolio/static/media/profileHeader.0c0557d9.png"
+  },
+  {
+    "revision": "b5c253011675d4c44cfe31f8340a99fb",
+    "url": "/psj-portfolio/static/media/swu.b5c25301.png"
+  }
+]);
